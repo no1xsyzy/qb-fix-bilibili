334b31eef7b48da0ba3ecd6fdfe9b55b5c57052e
Xu Siyuan <inqb@protonmail.com>
2022-03-17 09:12:03 +0800

    feat: 主页显示大关注栏

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import 分区 from './路由/分区'
 import 直播间 from './路由/直播间'
+import 直播主页 from './路由/直播主页'
 
-if (location.pathname === '/p/eden/area-tags') {
+if (location.pathname === '/') {
+  直播主页()
+} else if (location.pathname === '/p/eden/area-tags') {
   分区()
 } else if (/^\/\d+$/.exec(location.pathname)) {
   直播间()
---
+++ src/路由/直播主页.js
@@ -0,0 +1,5 @@
+import 关注栏尺寸 from '../功能/关注栏尺寸'
+
+export default function () {
+  关注栏尺寸()
+}
