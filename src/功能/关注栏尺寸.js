482a38a52438f7f1bc9c394f81fe428f9de5691a
Xu Siyuan <inqb@protonmail.com>
2022-03-17 09:11:36 +0800

    fix(关注栏尺寸)

--- src/功能/关注栏尺寸.js
+++ src/功能/关注栏尺寸.js
@@ -2,18 +2,18 @@ import { $ } from '../基本/selector'
 import { launchObserver } from '../基本/observer'
 
 export default function () {
-  GM_addStyle(`.section-content-cntr[data-v-649cf1f8]{height:calc(100vh - 250px)!important;}`)
+  GM_addStyle(`.section-content-cntr{height:calc(100vh - 250px)!important;}`)
 
   launchObserver({
-    selector: `#sidebar-vm .side-bar-popup-cntr.ts-dot-4`,
+    selector: `.side-bar-popup-cntr.ts-dot-4`,
     successCallback: () => {
-      const g = $`#sidebar-vm .side-bar-popup-cntr.ts-dot-4`
+      const g = $`.side-bar-popup-cntr.ts-dot-4`
       if (g.style.height !== '0px') {
         g.style.bottom = '75px'
         g.style.height = 'calc(100vh - 150px)'
         // g.style.height = "600px"
       }
-      setTimeout(() => $(`#sidebar-vm .side-bar-popup-cntr.ts-dot-4 .ps`).dispatchEvent(new Event('scroll')), 1000)
+      setTimeout(() => $(`.side-bar-popup-cntr.ts-dot-4 .ps`).dispatchEvent(new Event('scroll')), 1000)
     },
     stopWhenSuccess: false,
     config: {
