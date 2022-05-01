3de586c08fb8442383202df62a864e65f2dd5353
Xu Siyuan <inqb@protonmail.com>
2022-05-01 16:54:08 +0800

    build: use different file for debug build

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,37 @@
 import metablock from 'rollup-plugin-userscript-metablock'
 import typescript from '@rollup/plugin-typescript'
 import strip from '@rollup/plugin-strip'
-
-const pkg = require('./package.json')
-
-const metab = metablock({
-  file: './src/meta.json',
-  override: {
-    name: pkg.name,
-    description: pkg.description,
-    version: pkg.version,
-    homepage: pkg.homepage,
-    author: pkg.author,
-  },
-})
+import pkg from './package.json'
 
 export default (commandLineArgs) => {
+  const { configDebug } = commandLineArgs
+
   const input = 'src/main.ts'
   const output = {
-    file: 'qb-fix-bilibili.user.js',
+    file: configDebug ? 'qb-fix-bilibili-debug.user.js' : 'qb-fix-bilibili.user.js',
     format: 'iife',
     globals: {},
   }
-  const plugins = [metab, typescript()]
+
+  const plugins = [
+    metablock({
+      file: './src/meta.json',
+      override: {
+        name: configDebug ? pkg.name + '-debug' : pkg.name,
+        description: configDebug ? pkg.description + ' (debug version)' : pkg.description,
+        version: configDebug
+          ? pkg.version + '-' + new Date().toISOString().slice(0, 10).replace(/-?/g, '')
+          : pkg.version,
+        homepage: pkg.homepage,
+        author: pkg.author,
+      },
+    }),
+    typescript(),
+  ]
+
   const external = []
 
-  if (!commandLineArgs.configDebug) {
+  if (!configDebug) {
     plugins.push(strip({ include: '**/*.(js|ts)' }))
   }
 
