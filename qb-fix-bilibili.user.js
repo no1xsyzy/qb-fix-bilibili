1673e5fd648f29d8edefb86aba91fb68449becc3
Xu Siyuan <inqb@protonmail.com>
2022-07-04 23:33:09 +0800

    bumpver

--- qb-fix-bilibili.user.js
+++ qb-fix-bilibili.user.js
@@ -4,7 +4,7 @@
 // @namespace   no1xsyzy
 // @match       https://*.bilibili.com/*
 // @license     Apache License, Version 2.0 (Apache-2.0); https://opensource.org/licenses/Apache-2.0
-// @version     0.0.14
+// @version     0.0.15
 // @author      inQ_Beta
 // @grant       GM_addStyle
 // ==/UserScript==
