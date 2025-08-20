316744e22f0c24021837e9910583e54771c912c4
Xu Siyuan <inqb@protonmail.com>
2025-08-20 11:27:22 +0800

    remove: 分区标题

--- src/路由/分区.ts
+++ src/路由/分区.ts
@@ -1,11 +1,9 @@
 import 关注栏尺寸 from '../功能/关注栏尺寸'
-import 分区标题 from '../功能/分区标题'
 import 分区添加粉丝数 from '../功能/分区添加粉丝数'
 import 分区卡片直播间标题指向 from '../功能/分区卡片直播间标题指向'
 
 export default function () {
   关注栏尺寸()
-  分区标题()
   分区添加粉丝数()
   分区卡片直播间标题指向()
 }
